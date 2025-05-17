4ffcdd11f567e2ed269901d524baa5a8a1d6bfae
David Olowokere <davetwealth@gmail.com>
2025-05-17 12:46:37 -0500

    Nested if statement

---
+++ Nested if statement.py
@@ -0,0 +1,24 @@
+# What this is: Nested if statement.
+# Goal: conditional statement based on score and age.
+
+score = input("What was your test score? ")
+score = int(score)
+
+if score >= 90:
+    age = int(input("what is your age? "))
+    if age <= 10:
+        print("You got an A+ based on your age")
+    elif age > 10:
+        print("You got an A, congrats!")
+elif score >= 80:
+    age = int(input("what is your age? "))
+    if age <= 10:
+        print("You got an B+ based on your age")
+elif score >= 70:
+    age = int(input("what is your age? "))
+    if age <= 10:
+        print("You got an C+ based on your age")
+elif score >= 60:
+    print("You got a D, do better next time!")
+else:
+    print("you didnt pass, try again next year!")
